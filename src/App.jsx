ced5e2bfa554ddbdcae138bf0f1a7c9000bf12c0
Ed Duggan <eduggan2008@gmail.com>
2025-10-03 13:19:03 -0400

    Styled the ChatBotApp.css file, with some minor modifications to ChatBotApp.jsx and App.jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ChatBotStart from './Components/ChatBotStart'
+import ChatBotApp from './Components/ChatBotApp'
 
 const App = () => {
   return (
     <div className='container'>
-      <ChatBotStart />
+      {/* <ChatBotStart /> */}
+      <ChatBotApp />
     </div>
   )
 }
---
+++ src/Components/ChatBotApp.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import './ChatBotApp.css'
+
+const ChatBotApp = () => {
+  return (
+    <div className='chat-app'>
+      <div className='chat-list'>
+        <div className='chat-list-header'>
+          <h2>Chat List</h2>
+          <i className='bx bx-edit-alt new-chat'></i>
+        </div>
+        <div className='chat-list-item active'>
+          <h4>Chat 10/03/2025 11:44:42 AM</h4>
+          <i className='bx bx-x-circle'></i>
+        </div>
+        <div className='chat-list-item'>
+          <h4>Chat 10/03/2025 11:48:27 AM</h4>
+          <i className='bx bx-x-circle'></i>
+        </div>
+        <div className='chat-list-item'>
+          <h4>Chat 10/03/2025 11:54:04 AM</h4>
+          <i className='bx bx-x-circle'></i>
+        </div>
+        <div className='chat-list-item'>
+          <h4>Chat 10/03/2025 12:15:29 PM</h4>
+          <i className='bx bx-x-circle'></i>
+        </div>
+      </div>
+      <div className='chat-window'>
+        <div className='chat-title'>
+          <h3>Chat with AI</h3>
+          <i class='bx  bx-arrow-left arrow'  ></i> 
+        </div>
+        <div className='chat'>
+          <div className='prompt'>Hi, how are you? <span>11:44:42 PM</span></div>
+          <div className='response'>Hello, I am just a computer program so I don't have feelings.  However, I am here and ready to assist you. How can I help you today? <span>11:44:44 PM</span></div>
+          <div className='typing'>Typing...</div>
+        </div>
+        <form className='msg-form'>
+          <i className='fa-solid fa-face-smile emoji'></i>
+          <input type='text' className='msg-input' placeholder='Type a message' />
+          <i className='fa-solid fa-paper-plane'></i>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default ChatBotApp
